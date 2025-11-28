e57b9d07d7cbd99c7bd0fb1f6ba2480ef73a7692
b <66017805+bryanseah234@users.noreply.github.com>
2025-11-28 21:31:57 +0800

    Refactor main.py to remove duplicate header function
    Signed-off-by: b <66017805+bryanseah234@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-from shutil import Error
-from PIL import Image
-import time
-from flask import Flask, render_template, request, redirect, url_for
-from flask import *
-from flask import send_file, send_from_directory, safe_join, abort
-from copy import copy
-# from main import app as Application
-from werkzeug.utils import secure_filename
-
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
-
 
 @app.after_request
 def add_header(response):
@@ -20,13 +10,6 @@ def add_header(response):
     response.cache_control.max_age = 0
     return response
 
-
-@app.after_request
-def add_header(response):
-    response.cache_control.max_age = 0
-    return response
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET', 'POST'])
 def root():
     return render_template('index.html')
